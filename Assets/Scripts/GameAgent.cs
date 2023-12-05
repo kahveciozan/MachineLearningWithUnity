6f912b30e792eefffa5866dc8d2ce0a1f95d92fc
ozan kahveci <47081910+kahveciozan@users.noreply.github.com>
2023-12-05 23:05:07 +0300

    First commit

---
+++ Assets/Scripts/GameAgent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.PlasticSCM.Editor.UI;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class GameAgent : Agent
+{
+    
+}
